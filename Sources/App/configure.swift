e03b26a14f4e1d86b1e4b36b352e8ad7e8287a0e
Reid Chatham <reid.chatham@gmail.com>
2019-09-27 11:27:31 -0400

    fix; postgres conversion

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -38,6 +38,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .psql)
     services.register(migrations)
 }
