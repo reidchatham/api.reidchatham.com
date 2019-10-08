c54dac6270a354404aa7bb06565299e649a72c8f
Reid Chatham <reid.chatham@gmail.com>
2019-10-07 23:07:42 -0400

    add User.swift

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,29 @@
+import FluentPostgreSQL
+import Vapor
+import Authentication
+
+final class User: PostgreSQLModel {
+    var id: Int?
+    var email: String
+    var password: String
+
+    init(id: Int? = nil, email: String, password: String) {
+        self.id = id
+        self.email = email
+        self.password = password
+    }
+}
+
+extension User: Content {}
+extension User: Migration {}
+
+extension User: PasswordAuthenticatable {
+    static var usernameKey: WritableKeyPath<User, String> {
+        return \User.email
+    }
+    static var passwordKey: WritableKeyPath<User, String> {
+        return \User.password
+    }
+}
+
+extension User: SessionAuthenticatable {}
