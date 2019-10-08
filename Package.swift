b282fee43a37cd2ae84eee794848241e05bb4049
Reid Chatham <reid.chatham@gmail.com>
2019-10-07 23:07:12 -0400

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,9 +12,14 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on Postgresql.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Leaf", "Authentication", "Crypto"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
